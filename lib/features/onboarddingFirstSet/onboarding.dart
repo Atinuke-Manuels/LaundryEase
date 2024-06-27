d886c1d38a651457ecb988e20433c8f030d806c1
Cynthia Enweonwu-Arinze <ladycee15@yahoo.com>
2024-06-27 23:37:50 +0100

    implemented skip and next button

--- lib/features/onboarddingFirstSet/onboarding.dart
+++ lib/features/onboarddingFirstSet/onboarding.dart
@@ -22,6 +22,7 @@ class _onboardingState extends State<onboarding> {
 
 //keep track o the current page index
   int _currentPage = 0;
+  bool onLastPage = false;
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +34,7 @@ class _onboardingState extends State<onboarding> {
                 onPageChanged: (index) {
                   setState(() {
                     _currentPage = index;
+                    onLastPage = (index ==2);
                   });
                 },
                 children: [
@@ -44,19 +46,46 @@ class _onboardingState extends State<onboarding> {
 
                 //skip button
 
+              if(!onLastPage)
               Positioned(
                 top: 44,
                   right: 32,
                   child: GestureDetector(
-                    onTap: (){},
+                    onTap: (){
+                      _controller.jumpToPage(2);
+                    },
                     child: Text("Skip",
                       style: TextStyle(fontSize: 16, color: Color(0xFF0F26A6)),
                     ),
                   ),
               ),
+              Positioned(
+                bottom: 80,
+                  left: 150,
+                  child: CustomButton(
+                    title: onLastPage ? "Done" : "Next",
+                    padding: EdgeInsets.symmetric(horizontal:32, vertical: 8),
+                    backgroundColor: Color(0xFF0F26A6),
+                    foregroundColor: Colors.white,
+                    onPressed: (){
+                      if (onLastPage) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => GetStarted())
+                        );
+                      } else {
+                        _controller.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn,
+                        );
+                      }
+
+                    },
+                  ),
+              ),
               //dot indicators
               Container(
-                  alignment: Alignment(0,0.7),
+                  alignment: Alignment(0,0.6),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
@@ -72,41 +101,85 @@ class _onboardingState extends State<onboarding> {
                         ),
                       ),
 
-                      SizedBox(height:MediaQuery.of(context).size.height * 0.02),
+                      SizedBox(height:MediaQuery.of(context).size.height * 0.03),
 
                       if (_currentPage == 0)
                         Column(
                           children: [
-                            Text("Effortless Laundry",
-                            style: TextStyle(color: Colors.black),
-                            ),
-                            Text("Schedule. Track and Relax with LaundryEase!"),
+                            RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: 'Effort',
+                                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                                  ),
+                                  TextSpan(
+                                    text: 'less',
+                                    style: TextStyle(color: Color(0xFF0F26A6), fontWeight: FontWeight.bold),
+                                  ),
+
+                                  TextSpan(
+                                    text: ' laundry',
+                                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                               ),
+                              ),
+                            SizedBox(height:MediaQuery.of(context).size.height * 0.01),
+                           Text("Schedule, Track and Relax with LaundryEase!"),
                             SizedBox(height:MediaQuery.of(context).size.height * 0.04),
-                            CustomButton(
-                              title: "Next",
-                              padding: EdgeInsets.symmetric(horizontal:32, vertical: 8),
-                              backgroundColor: Color(0xFF0F26A6),
-                              foregroundColor: Colors.white,
-                              onPressed: (){},
-                            )
+                            // CustomButton(
+                            //   title: "Next",
+                            //   padding: EdgeInsets.symmetric(horizontal:32, vertical: 8),
+                            //   backgroundColor: Color(0xFF0F26A6),
+                            //   foregroundColor: Colors.white,
+                            //   onPressed: (){
+                            //     _controller.nextPage(
+                            //       duration: Duration(milliseconds: 500),
+                            //       curve: Curves.easeIn,
+                            //     );
+                            //   },
+                            // )
                           ],
                         ),
 
                       if (_currentPage == 1)
                         Column(
                           children: [
-                            Text("Convenient & Quality Care",
-                              style: TextStyle(color: Colors.black),
+                            RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: 'Convenient & ',
+                                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                                  ),
+                                  TextSpan(
+                                    text: 'Quality ',
+                                    style: TextStyle(color: Color(0xFF0F26A6), fontWeight: FontWeight.bold),
+                                  ),
+
+                                  TextSpan(
+                                    text: 'Care',
+                                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
                             ),
+                            SizedBox(height:MediaQuery.of(context).size.height * 0.01),
                             Text("Professional clean laundry at your doorstep"),
                             SizedBox(height:MediaQuery.of(context).size.height * 0.04),
-                            CustomButton(
-                              title: "Next",
-                              padding: EdgeInsets.symmetric(horizontal:32, vertical: 8),
-                              backgroundColor: Color(0xFF0F26A6),
-                              foregroundColor: Colors.white,
-                              onPressed: (){},
-                            )
+                            // CustomButton(
+                            //   title: "Next",
+                            //   padding: EdgeInsets.symmetric(horizontal:32, vertical: 8),
+                            //   backgroundColor: Color(0xFF0F26A6),
+                            //   foregroundColor: Colors.white,
+                            //   onPressed: (){
+                            //     _controller.nextPage(
+                            //       duration: Duration(milliseconds: 500),
+                            //       curve: Curves.easeIn,
+                            //     );
+                            //   },
+                            // )
 
                           ],
                         ),
@@ -114,22 +187,39 @@ class _onboardingState extends State<onboarding> {
                       if (_currentPage == 2)
                         Column(
                           children: [
-                            Text("Reliable laundry Professionals",
-                              style: TextStyle(color: Colors.black),
+                            RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: 'Reliable ',
+                                    style: TextStyle(color: Color(0xFF0F26A6), fontWeight: FontWeight.bold),
+                                  ),
+                                  TextSpan(
+                                    text: 'laundry',
+                                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                                  ),
+
+                                  TextSpan(
+                                    text: ' Professionals',
+                                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
                             ),
+                            SizedBox(height:MediaQuery.of(context).size.height * 0.01),
                             Text("Connect with reliable laundry professionals near you"),
                             SizedBox(height:MediaQuery.of(context).size.height * 0.04),
-                            CustomButton(
-                                title: "Next",
-                                padding: EdgeInsets.symmetric(horizontal:32, vertical: 8),
-                                backgroundColor: Color(0xFF0F26A6),
-                                foregroundColor: Colors.white,
-                                onPressed: (){
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(builder: (context) => GetStarted()));
-                                },
-                            )
+                            // CustomButton(
+                            //     title: "Next",
+                            //     padding: EdgeInsets.symmetric(horizontal:32, vertical: 8),
+                            //     backgroundColor: Color(0xFF0F26A6),
+                            //     foregroundColor: Colors.white,
+                            //     onPressed: (){
+                            //       Navigator.push(
+                            //           context,
+                            //           MaterialPageRoute(builder: (context) => GetStarted()));
+                            //     },
+                            // )
                           ],
                         ),
 
